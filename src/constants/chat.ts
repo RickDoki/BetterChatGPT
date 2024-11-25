ff823264d0c28c8ed762e026e883a01d16a5ae56
RickDoki <rickandroll@foxmail.com>
2024-11-25 14:46:06 +0800

    Update chat.ts

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -30,7 +30,7 @@ export const modelOptions: ModelOptions[] = [
   'gpt-4-turbo-2024-04-09',
   'gpt-4o',
   'gpt-4o-2024-05-13',
-  'gpt-4o-111',
+  'gpt-4o-2024-08-06',
   // 'gpt-3.5-turbo-0301',
   // 'gpt-4-0314',
   // 'gpt-4-32k-0314',
@@ -58,7 +58,7 @@ export const modelMaxToken = {
   'gpt-4-turbo-2024-04-09': 128000,
   'gpt-4o': 128000,
   'gpt-4o-2024-05-13': 128000,
-  'gpt-4o-111': 128000,
+  'gpt-4o-2024-08-06': 128000,
 };
 
 export const modelCost = {
@@ -138,7 +138,7 @@ export const modelCost = {
     prompt: { price: 0.005, unit: 1000 },
     completion: { price: 0.015, unit: 1000 },
   },
-  'gpt-4o-111': {
+  'gpt-4o-2024-08-06': {
     prompt: { price: 0.005, unit: 1000 },
     completion: { price: 0.015, unit: 1000 },
   },
